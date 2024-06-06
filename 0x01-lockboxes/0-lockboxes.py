8c946a1a77068ae701fb697cad7082c616a3347a
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-06 19:13:15 +0100

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,11 +8,32 @@ def canUnlockAll(boxes):
         return False
     if len(boxes) == 0:
         return False
-    keys = [0]
-    for key in keys:
-        if key >= len(boxes) or boxes[key] is None:
-            continue
-        for i in boxes[key]:
-            if i not in keys and i < len(boxes):
-                keys.append(i)
-    return len(keys) == len(boxes)
+    queue = [0]
+    setElements = set([0])
+    while True:
+        value = queue.pop(0)
+        tempList = boxes[value]
+        setElements.add(value)
+        for temp in tempList:
+            if temp not in setElements:
+                queue.append(temp)
+        if len(queue) == 0:
+            break
+    if len(setElements) == len(boxes):
+        return True
+    return False
+
+# def canUnlockAll(boxes):
+#     """Function that determines if all the boxes can be opened"""
+#     if boxes is None:
+#         return False
+#     if len(boxes) == 0:
+#         return False
+#     keys = [0]
+#     for key in keys:
+#         if key >= len(boxes) or boxes[key] is None:
+#             continue
+#         for i in boxes[key]:
+#             if i not in keys and i < len(boxes):
+#                 keys.append(i)
+#     return len(keys) == len(boxes)
