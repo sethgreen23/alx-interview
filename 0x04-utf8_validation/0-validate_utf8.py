5e618bbe5f2609f2c399f7e65a70132dca02195f
Dridi Chaith <dridi.chaith@gmail.com>
2024-06-26 23:18:40 +0100

    task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,34 +12,44 @@ def num_bin(number):
     return binary_repr.zfill(8)
 
 
-def validUTF8(data):
-    """Determine if the given data set represents a valid UTF-8 encoding."""
-    least_bit_hash = {
-        '0': 0,
-        '10': 1,
-        '110': 2,
-        '1110': 3,
-        '11110': 4
-    }
-    if len(data) == 0:
-        return False
-    first_character = num_bin(data[0])
-    valid_first_bytes = True
-    count = 1
-    for b in sorted(least_bit_hash.keys()):
-        if first_character[:count] != b:
-            valid_first_bytes = False
-        if first_character[:count] == b:
-            key = b
-            valid_first_bytes = True
+def get_num_bytes(byte):
+    """Get the number of bytes for a character"""
+    binary = num_bin(byte)
+    if binary[0] == '0':
+        return 1
+    count = 0
+    for bit in binary:
+        if bit == '1':
+            count += 1
+        else:
             break
-        count += 1
-    if not valid_first_bytes:
-        return False
-    # if len(data) < least_bit_hash[key]:
-    #     return False
-    for test_num in data[1:]:
-        item = num_bin(test_num)
-        if item[:2] != '10' or test_num == 256:
+    return count if 1 < count <= 4 else 0
+
+
+def is_continuation_byte(byte):
+    """Check if a byte is a continuation byte"""
+    return num_bin(byte)[:2] == '10'
+
+
+def validUTF8(data):
+    """Validate utf8"""
+    i = 0
+    while i < len(data):
+        # Get the number of bytes for this character
+        num_bytes = get_num_bytes(data[i])
+        if num_bytes == 0:
             return False
+
+        # Check if there are enough bytes left in data
+        if i + num_bytes > len(data):
+            return False
+
+        # Validate the correct number of continuation bytes
+        for j in range(i + 1, i + num_bytes):
+            if not is_continuation_byte(data[j]):
+                return False
+
+        # Move i to the start of the next character
+        i += num_bytes
+
     return True
