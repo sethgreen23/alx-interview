4be7961ada9cf94f2b8818a2227030a04b6b3047
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-23 23:18:21 +0100

    task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    index = 0
+    coins_arranged = sorted(coins, reverse=True)
+    result = 0
+    while (True):
+        while (total >= coins_arranged[index]):
+            result += 1
+            total -= coins_arranged[index]
+        if (total < 0):
+            return -1
+        if total == 0:
+            return result
+        index += 1
+        if index >= len(coins_arranged):
+            return -1
