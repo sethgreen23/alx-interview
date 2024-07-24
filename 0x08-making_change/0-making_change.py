4246282806436b14f6d5d9bb4984b4191eec6db1
Dridi Chaith <dridi.chaith@gmail.com>
2024-07-24 01:21:27 +0100

    task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,17 +7,27 @@ Main file for testing
 def makeChange(coins, total):
     if total <= 0:
         return 0
-    index = 0
-    coins_arranged = sorted(coins, reverse=True)
-    result = 0
-    while (True):
-        while (total >= coins_arranged[index]):
-            result += 1
-            total -= coins_arranged[index]
-        if (total < 0):
-            return 0
-        if total == 0:
-            return result
-        index += 1
-        if index >= len(coins_arranged):
-            return -1
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total] if dp[total] < total else -1
+# def makeChange(coins, total):
+#     if total <= 0:
+#         return 0
+#     index = 0
+#     coins_arranged = sorted(coins, reverse=True)
+#     result = 0
+#     while (True):
+#         while (total >= coins_arranged[index]):
+#             result += 1
+#             total -= coins_arranged[index]
+#         if (total < 0):
+#             return 0
+#         if total == 0:
+#             return result
+#         index += 1
+#         if index >= len(coins_arranged):
+#             return -1
